c9838305d5c79e82fb16322a6acee85be6e73213
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-01-02 09:15:57 +0100

    chore: new refacto on sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,46 +3,46 @@ import type { MetadataRoute } from 'next'
 const baseUrl = process.env.BASE_LINK
 
 const paths: MetadataRoute.Sitemap = [
-	{
-		url: ``,
-		changeFrequency: 'yearly',
-		priority: 1,
-	},
-	{
-		url: '/services',
-		changeFrequency: 'monthly',
-		priority: 0.8,
-	},
-	{
-		url: '/resume',
-		changeFrequency: 'monthly',
-		priority: 0.8,
-	},
-	{
-		url: '/resume/educations',
-		changeFrequency: 'monthly',
-		priority: 0.6,
-	},
-	{
-		url: '/resume/experiences',
-		changeFrequency: 'monthly',
-		priority: 0.6,
-	},
-	{
-		url: '/resume/about',
-		changeFrequency: 'monthly',
-		priority: 0.6,
-	},
-	{
-		url: '/projects',
-		changeFrequency: 'monthly',
-		priority: 0.8,
-	},
-	{
-		url: '/contact',
-		changeFrequency: 'monthly',
-		priority: 0.8,
-	},
+   {
+      url: ``,
+      changeFrequency: 'monthly',
+      priority: 1,
+   },
+   {
+      url: '/services',
+      changeFrequency: 'weekly',
+      priority: 0.8,
+   },
+   {
+      url: '/resume',
+      changeFrequency: 'weekly',
+      priority: 0.8,
+   },
+   {
+      url: '/resume/educations',
+      changeFrequency: 'weekly',
+      priority: 0.6,
+   },
+   {
+      url: '/resume/experiences',
+      changeFrequency: 'weekly',
+      priority: 0.6,
+   },
+   {
+      url: '/resume/about',
+      changeFrequency: 'weekly',
+      priority: 0.6,
+   },
+   {
+      url: '/projects',
+      changeFrequency: 'weekly',
+      priority: 0.8,
+   },
+   {
+      url: '/contact',
+      changeFrequency: 'weekly',
+      priority: 0.8,
+   },
 ]
 
 export default function sitemap(): MetadataRoute.Sitemap {
